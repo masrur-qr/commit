45bb3432ca16184b96e38c5771ae15c99000c89d
masrur <you@example.com>
2024-01-02 20:22:03 +0500

    Auto commit at: 2024-01-02 20:22:03

--- main.go
+++ main.go
@@ -1,88 +1,74 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"log"
 	"os"
 	"os/exec"
 	"time"
-
-	"github.com/google/go-github/github"
-	"golang.org/x/oauth2"
 )
 
 // GitHub credentials
 const (
-	username    = "masrur-qr"
-	repoName    = "commit"
-	accessToken = "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"
+	username     = "masrur-qr"
+	repoName     = "commit"
+	accessToken  = "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"
+	repoDirectory = "./"
 )
 
-func createCommitAndPush(client *github.Client) {
+// GitHub API URL
+var  apiURL = fmt.Sprintf("https://%v@github.com/%v/%v.git",accessToken,username,repoName)
+// const apiURL = "https://%vgithub.com/masrur-qr/commit.git"
+
+func createCommitAndPush() {
 	// Change to the repository directory
-	os.Chdir(repoName)
+	os.Chdir(repoDirectory)
+	fmt.Println("hello")
 
 	// Read the content of a sample file (you can customize this part)
 	file, err := os.OpenFile("sample_file.txt", os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
-		log.Fatal("Error opening file:", err)
+		fmt.Println("Error opening file:", err)
+		return
 	}
 	defer file.Close()
 
 	_, err = file.WriteString("\nCommit made at: " + time.Now().Format("2006-01-02 15:04:05"))
 	if err != nil {
-		log.Fatal("Error writing to file:", err)
+		fmt.Println("Error writing to file:", err)
+		return
 	}
 
 	// Stage and commit changes
 	cmd := exec.Command("git", "add", ".")
 	err = cmd.Run()
 	if err != nil {
-		log.Fatal("Error staging changes:", err)
+		fmt.Println("Error staging changes:", err)
+		return
 	}
 
 	cmd = exec.Command("git", "commit", "-m", fmt.Sprintf("Auto commit at: %s", time.Now().Format("2006-01-02 15:04:05")))
 	err = cmd.Run()
 	if err != nil {
-		log.Fatal("Error committing changes:", err)
+		fmt.Println("Error committing changes:", err)
+		return
 	}
 
-	
-	// Now create a commit using the GitHub API
-	commitMessage := fmt.Sprintf("Auto commit at: %s", time.Now().Format("2006-01-02 15:04:05"))
-	opts := &github.RepositoryContentFileOptions{
-		Content: []byte(commitMessage),
-		Message: github.String(commitMessage),
-	}
-	
-	_, _, err = client.Repositories.CreateFile(context.Background(), username, repoName, "sample_file.txt", opts)
-	if err != nil {
-		log.Fatal("Error creating commit via GitHub API:", err)
-	}
 	// Push to the repository
-	cmd = exec.Command("git", "push", "origin", "golang")
+	cmd = exec.Command("git", "push", apiURL , "golang")
 	err = cmd.Run()
 	if err != nil {
-		log.Fatal("Error pushing changes:", err)
+		fmt.Println("Error pushing changes:", err)
+		return
 	}
 }
 
-func job(client *github.Client) {
+func job() {
 	fmt.Println("Creating commit and pushing to GitHub...")
-	createCommitAndPush(client)
+	createCommitAndPush()
 	fmt.Println("hello")
 }
 
 func main() {
-	ctx := context.Background()
-	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: accessToken},
-	)
-	tc := oauth2.NewClient(ctx, ts)
-
-	client := github.NewClient(tc)
-
 	// Schedule the job to run every hour
 	ticker := time.NewTicker(time.Second)
 	defer ticker.Stop()
@@ -90,7 +76,7 @@ func main() {
 	for {
 		select {
 		case <-ticker.C:
-			job(client)
+			job()
 		}
 	}
 }
